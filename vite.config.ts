2b2529473d7a119cd6b1e8f4ab0f584e4b53c35e
Ruan De Villiers <Ruan.DeVilliers@nintex.com>
2025-07-18 05:58:18 +0200

    chore: udpated

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,9 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
+// Set base to '/kids/' for GitHub Pages deployment
 export default defineConfig({
+  base: '/kids/',
   plugins: [react()],
-  css: {
-    modules: {
-      localsConvention: 'camelCaseOnly',
-    },
-  },
 });
